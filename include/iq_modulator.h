4655b3a11190f4a7db235953130863dbab3c643c
anpinto <anp@ua.pt>
2016-06-20 17:50:48 +0100

    Optical Power

--- include/iq_modulator.h
+++ include/iq_modulator.h
@@ -1,7 +1,9 @@
 # ifndef PROGRAM_INCLUDE_IQ_MODULATOR_H_
 # define PROGRAM_INCLUDE_IQ_MODULATOR_H_
 
+# include <math.h>       /* pow */
 # include "netplus.h"
+
 
 // Implements a IQ modulator.
 class IqModulator : public Block {
@@ -17,6 +19,7 @@ class IqModulator : public Block {
 	 bool runBlock(void);
 
 	 void setOutputOpticalPower(t_real outOpticalPower) { outputOpticalPower = outOpticalPower; }
+	 void setOutputOpticalPower_dBm(t_real outOpticalPower_dBm) { outputOpticalPower = 1e-3*pow(10,outOpticalPower_dBm/10); }
 };
 
 # endif // PROGRAM_INCLUDE_NETPLUS_H_
