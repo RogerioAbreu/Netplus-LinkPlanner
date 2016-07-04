b37ad2c32737920a80b67174d8bb8717d1c19981
anpinto <anp@ua.pt>
2016-07-04 11:54:17 +0100

    t

--- sdf/m_qam_system/m_qam_transmitter_sdf.cpp
+++ sdf/m_qam_system/m_qam_transmitter_sdf.cpp
@@ -21,7 +21,7 @@ int main(){
 	B1.setNumberOfBits(10000);
 	B1.setNumberOfSamplesPerSymbol(16);
 	B1.setRollOffFactor(0.3);
-	B1.setIqAmplitudes({ { 1, 0 }, { -1, 0 } });
+	B1.setIqAmplitudes({ { 1, 1 }, { -1, 1 }, { -1, -1 }, { 1, -1 } });
 	B1.setOutputOpticalPower_dBm(0);
 	B1.setSaveInternalSignals(true);
 
